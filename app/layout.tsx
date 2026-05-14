26cf243c366dc21438b965a71762db6c32cd6ded
BoukaizMakram <zidane.makram@gmail.com>
2026-05-14 23:04:05 +0100

    google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -44,6 +45,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-5M2M6LYXHF"
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-5M2M6LYXHF');`}
+        </Script>
+      </head>
       <body suppressHydrationWarning>{children}</body>
     </html>
   );
